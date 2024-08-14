cbe61ca9a2d9c2a1b7150239170f365cbd3eac93
mr-ray <fedjioraymond@gmail.com>
2024-08-14 11:44:07 +0100

    Add dropdownmenu to show user

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,12 @@
 "use client";
-import { Box, Container, Flex } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -42,8 +49,28 @@ const NavBar = () => {
           </Flex>
           <Box>
             {status === "authenticated" ? (
-              <Link href="/api/auth/signout">Logout</Link>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    size="2"
+                    fallback="?"
+                    radius="full"
+                    className="cursor-pointer"
+                    src={session.user!.image!}
+                  />
+                </DropdownMenu.Trigger>
+
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="2">{session.user!.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Logout</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
             ) : (
+              // <Link href="/api/auth/signout">Logout</Link>
               <Link href="/api/auth/signin">Login</Link>
             )}
           </Box>
