3ecc11cdc8576aaada9d3f8b0785ab1c50c3bb71
mr-ray <fedjioraymond@gmail.com>
2024-08-15 13:03:25 +0100

    Filter Issues

--- app/issues/(issues)/IssueActions.tsx
+++ app/issues/(issues)/IssueActions.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
-import { Button } from "@radix-ui/themes";
+import { Button, Flex } from "@radix-ui/themes";
+import IssueStaticFilter from "./IssueStatusFilter";
 
 const IssueActions = () => {
   return (
-    <div className="mb-5">
+    <Flex mb="5" justify="between">
+      <IssueStaticFilter />
       <Button>
         <Link href="/issues/new">New Issue</Link>
       </Button>
-    </div>
+    </Flex>
   );
 };
 
